1dfc569fda743528ed9056536090a842f85602ad
胡春 <huchun@extracme.com>
2019-06-23 12:18:20 +0800

    redux+TS 基本架构搭建

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -1,20 +1,59 @@
 import * as React from "react";
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
+import { connect } from "react-redux";
+import { addOne, test } from "../redux/actions";
 
-interface propTypes {
-    testInterface?: string
+interface IProps {
+    testInterface?: string,
+    addOne: Function,
+    test: Function,
+    goods: number,
+    compiler: string
+}
+export interface IState {
+    aaa: number,
 }
 
-export default class AppContainer extends React.Component<propTypes, {}>{
+class AppContainer extends React.Component<IProps, IState>{
     constructor (props: any) {
         super(props);
+    }
+    // 组件中使用默认的 Props
+    static defaultProps = {
+        compiler: 'TypeScript'
+    };
+
+    readonly state = {
+        aaa: 1,
+    }
+    public handleAddOne = () => {
+        this.props.addOne();
+        this.setState((preState, preProps) => {
+            return {aaa: preState.aaa + 1}
+        })
+        // this.props.test({
+        //     username: 'huchun',
+        //     password: '123456'
+        // });
     }
     render() {
         console.log(this.props)
         return (
             <View>
-                <Text>container</Text>
+                <Text>containers</Text>
+                <Text>goods: {this.props.goods}</Text>
+                <Text>state: {this.state.aaa}</Text>
+                <Text>{this.props.compiler}</Text>
+                <Button onPress={this.handleAddOne} title="Add One"/>
             </View>
         )
     }
 }
+const mapStateToProps = (state: any) => {
+    return {
+        goods: state.goods
+    }
+};
+const mapDispatchToProps = {addOne, test};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AppContainer)
--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,22 @@ import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
 import AppContainer from "./containers";
 import rootReducer from "./redux/reducers";
-// import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
 
 const store = createStore(
   rootReducer,
-  // composeWithDevTools(
-      applyMiddleware(
-          thunkMiddleware
-      )
-  // )
+  composeWithDevTools(
+    applyMiddleware(
+        thunkMiddleware
+    )
+  )
 )
-
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <AppContainer />
+        <AppContainer testInterface="testaaa"/>
       </Provider>
     );
   }
